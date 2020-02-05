41d9a1fa91e2af1a4153dfc5326b276a2d7839fd
Max Feldmann <maxfeldmann.consultant@gmail.com>
2020-02-05 17:44:46 +0100

    Added initialize and Methods to handle current Players

--- game.rb
+++ game.rb
@@ -1,4 +1,38 @@
+require 'set'
+
 class Game
+    ALPHABET = Set.new ("a".."z")
 
+    def initialize(*players)
+        words = File.readlines("dictionary.txt").map(&:chomp)
+        @dictionary = ["a", "b", "c"] #Set.new(words)
+        @players = players
+        @fragment = ""
+    end
 
+    attr_reader :dictionary, :players, :fragment
+
+    def game_over?(fragment)
+        @dictionary.include?(fragment)
+    end
+
+    def current_player
+        @players.first
+    end
+
+    def previous_player
+        @players[-1]
+    end
+
+    def next_player!
+        @players.rotate!
+    end
+
+    def valid_play?(letter) 
+        return false unless ALPHABET.include?(letter.downcase)                 # return false, if alphabet does not include the letter. downcased, because alphabet-set is completely lowercase.
+        return false if letter.
+
+        potential_fragment = @fragment + letter                             # potential fragment is existing fragment plus letter put in by play
+        @dictionary.any? {|word| word.start_with?(potential_fragment)}      # goes through dictionary and checks every word. if any word starts_with? the potential fragment returns true. if not false
+    end
 end
